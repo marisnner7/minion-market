008f8243a67d955cb481a4f52132ba694909c5ec
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-02 15:36:16 -0300

    helps generate with spec

---
+++ app/helpers/orders_to_products_helper.rb
@@ -0,0 +1,2 @@
+module OrdersToProductsHelper
+end
---
+++ spec/helpers/orders_to_products_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the OrdersToProductsHelper. For example:
+#
+# describe OrdersToProductsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe OrdersToProductsHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
