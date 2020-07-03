9002f935389a2ddb18386ec3a5c4b393e091d792
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-03 19:23:19 -0300

    validation to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :product
   belongs_to :user
+  validates :description, length: { maximum: 200 }
+  validates :rating, presence: true, acceptance: { accept: (1..5) }
 end
