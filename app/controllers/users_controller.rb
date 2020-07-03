1befced90ad31c10bcdc767eb44c646595fb4349
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-03 19:22:34 -0300

    markers for maps

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
-  before_action :find_user, only:; [:show]
+  before_action :find_user, only: [:show]
 
   def show
     authorize @user
-    @orders = Orders.where(user_id: @user.id)
-    @products = Products.where(user_id: @user.id)
-    @purchased_orders = @orders.each { |order| order.status == 'bought'}
+    @orders = Order.where(user_id: @user.id)
+    @products = Product.where(user_id: @user.id)
+    @purchased_orders = @orders.each { |order| order.status == "bought"}
 
-
+     @markers = [{
+      lat: @user.latitude,
+      lng: @user.longitude,
+      infoWindow: render_to_string(partial: "info_window", locals: { user: @user }),
+      image_url: helpers.asset_url('logo.png')
+    }]
   end
 
   private
